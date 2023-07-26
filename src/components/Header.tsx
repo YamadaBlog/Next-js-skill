f7ee81ec71512e7e7b85bdc73170257010ced7f7
Yamada <yamadaablog@gmail.com>
2023-07-26 20:36:32 +0200

    Navbar ready to be used

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,32 @@
+import Link from 'next/link';
+
 export const Header:React.FC = ()  => { 
-    return <div>header</div>;
+    return <header className="navbar">
+        <div className="navbar-start px-3">
+            <Link href="/">
+            LOGO
+            </Link>
+        </div>
+
+        <div className="navbar-end">
+            <div className="px-3">
+                <Link href="/blog">
+                    Blog
+                </Link>
+            </div>
+
+            <div className="px-3">
+                <Link href="/blog">
+                    xepextract
+                </Link>
+            </div>
+
+            <div className="px-3">
+                <Link href="/blog">
+                    Connexion
+                </Link>
+            </div>
+        </div>
+
+        </header>;
 }
